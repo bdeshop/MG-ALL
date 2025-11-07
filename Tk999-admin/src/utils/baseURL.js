f63f558906d4d84bb2ec0fa5fa20323f71164bc3
roni9843 <ronironi98439843@gmail.com>
2025-11-07 23:37:53 +0600

    addede all

---
+++ Tk999-admin/src/utils/baseURL.js
@@ -0,0 +1,3 @@
+export const baseURL = `${process.env.REACT_APP_BACKEND_API}api/v1/admin`;
+export const baseURL_For_IMG_UPLOAD = `${process.env.REACT_APP_BACKEND_API}upload`;
+export const baseURL_For_IMG_DELETE = `${process.env.REACT_APP_BACKEND_API}delete-image`;
